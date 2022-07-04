d187732c4b1e1158d8f5a6cffa470b75ad0386b4
Kash5622 <105967654+Kash5622@users.noreply.github.com>
2022-07-04 12:08:51 +0530

    Update settings.py

--- ecommarce/ecommerce1/ecommerce/settings.py
+++ ecommarce/ecommerce1/ecommerce/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u6dbn7_#mm2#cpd#bm3*fc()7*6!@-g8qgth1q^wfs7w^qdzic'
+# SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = []
 
